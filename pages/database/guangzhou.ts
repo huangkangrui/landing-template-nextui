77ca9d122459eb82567543e65abc20e7eca65ed3
huangkangrui <1505207242@qq.com>
2024-08-10 00:05:23 +0800

    静态资料

---
+++ pages/database/guangzhou.ts
@@ -0,0 +1,394 @@
+
+
+const girls =  [
+  {
+    id: 'gz1',
+    title: '小小 𝐇𝐞𝐢𝐠𝐡𝐭 身高170 • 𝐖𝐞𝐢𝐠𝐡𝐭体重/90', 
+    description: `
+    • 𝐁𝐮𝐬𝐭胸围/D             
+    • 𝐀𝐠𝐞年龄/04 短期兼职.声音甜美，🥢腿，完美身材 •待客热情 身体柔软 配合度高💯 
+    • 不催不事 乖巧听话 • 实力喜神 好评神器`, url: '/static/preview/1.jpg',
+    image: [
+      '/static/guangzhou/gz1/1.jpg',
+      '/static/guangzhou/gz1/2.jpg',
+      '/static/guangzhou/gz1/3.jpg',
+      '/static/guangzhou/gz1/4.jpg',
+      '/static/guangzhou/gz1/5.jpg',
+      '/static/guangzhou/gz1/6.jpg',      
+    ],
+    video: ['/static/guangzhou/gz1/7.mp4', ]
+  },
+  {
+    id: 'gz2',
+    title: '汤院 03 171C ', 
+    description: `
+    💜蜜桃臀多汁 软糯 与周海媚相似80％ 
+    💙 171  50公斤 03年 柳腰细腿 
+    💚H好不事   女友值爆棚 
+    💞涵养素质极高  期待与你相遇`, 
+    image: [
+      '/static/guangzhou/gz2/1.jpg',
+      '/static/guangzhou/gz2/2.jpg',
+      '/static/guangzhou/gz2/3.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz2/4.mp4',
+      '/static/guangzhou/gz2/5.mp4',
+      '/static/guangzhou/gz2/6.mp4',
+     ]
+  },
+  {
+    id: 'gz3',
+    title: '小粉 175- E🐻', 
+    description: `
+    小红书博主-店铺主理人 偶尔兼职 遇见便是好运😍`, 
+    image: [
+      '/static/guangzhou/gz3/1.jpg',
+      '/static/guangzhou/gz3/2.jpg',
+      '/static/guangzhou/gz3/3.jpg',
+      '/static/guangzhou/gz3/4.jpg',
+      '/static/guangzhou/gz3/5.jpg',
+      '/static/guangzhou/gz3/6.jpg',
+
+    
+    ],
+    video: [
+      '/static/guangzhou/gz3/7.mp4',
+      '/static/guangzhou/gz3/8.mp4',
+     ]
+  },
+  {
+    id: 'gz4',
+    title: '新疆妹 真实02年   172真🐻D🍬', 
+    description: `
+   舞蹈学院在校学生，纯兼职 全身雪白 欢迎体验 无一丝风尘味 
+   ⭐ 高素质 肤皮‬非常白滑五官精致                
+   ⭐ 涩羞‬可爱 乖听巧‬话 气质甜美款 
+   ⭐ 年轻💦 多 👇 紧面‬致有弹性`, 
+    image: [
+      '/static/guangzhou/gz4/1.jpg',
+      '/static/guangzhou/gz4/2.jpg',
+      '/static/guangzhou/gz4/3.jpg',
+      '/static/guangzhou/gz4/4.jpg',
+      '/static/guangzhou/gz4/5.jpg',
+      '/static/guangzhou/gz4/6.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz4/7.mp4',
+      '/static/guangzhou/gz4/8.mp4',
+      '/static/guangzhou/gz4/9.mp4',
+     ]
+  },
+  {
+    id: 'gz5',
+    title: '悦悦  03年166天然d', 
+    description: `
+    兼职短开`, 
+    image: [
+      '/static/guangzhou/gz5/1.jpg',
+      '/static/guangzhou/gz5/2.jpg',
+      '/static/guangzhou/gz5/3.jpg',
+      '/static/guangzhou/gz5/4.jpg',
+      '/static/guangzhou/gz5/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz5/6.mp4',
+      '/static/guangzhou/gz5/7.mp4',
+
+     ]
+  },
+  {
+    id: 'gz6',
+    title: 'Cindy IN ＃GUANGZHOU＃ 甜美少女う小清新', 
+    description: `
+    初恋女朋友 可盐可甜 性格nice 175cm  04年.  🐻c  A4腰 👸氧气甜妹，初恋女朋友feel. 清纯与精致兼具，美貌与气质共存`, 
+    image: [
+      '/static/guangzhou/gz5/1.jpg',
+      '/static/guangzhou/gz5/2.jpg',
+      '/static/guangzhou/gz5/3.jpg',
+      '/static/guangzhou/gz5/4.jpg',
+      '/static/guangzhou/gz5/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz5/6.mp4',
+      '/static/guangzhou/gz5/7.mp4',
+      '/static/guangzhou/gz5/8.mp4',
+     ]
+  },
+  {
+    id: 'gz7',
+    title: '少数民族延吉女孩', 
+    description: `
+    广州五福临门🏠广州❤️人 慢慢 武汉音乐学院
+    `, 
+    image: [
+      '/static/guangzhou/gz7/1.jpg',
+      '/static/guangzhou/gz7/2.jpg',
+      '/static/guangzhou/gz7/3.jpg',
+      '/static/guangzhou/gz7/4.jpg',
+      '/static/guangzhou/gz7/5.jpg',
+      '/static/guangzhou/gz7/6.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz7/7.mp4',
+      '/static/guangzhou/gz7/8.mp4',
+      '/static/guangzhou/gz7/9.mp4',
+    ]
+  },
+  {
+    id: 'gz8',
+    title: '175D御姐', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz8/1.jpg',
+      '/static/guangzhou/gz8/2.jpg',
+    ],
+    video: [
+      '/static/guangzhou/gz8/3.mp4',
+      '/static/guangzhou/gz8/4.mp4',
+      '/static/guangzhou/gz8/5.mp4',
+     ]
+  },
+  {
+    id: 'gz9',
+    title: '176车模', 
+    description: `
+    69蛇纹，代开，没纹身，有空姐服
+    `, 
+    image: [
+      '/static/guangzhou/gz9/1.jpg',
+      '/static/guangzhou/gz9/2.jpg',
+      '/static/guangzhou/gz9/3.jpg',
+      '/static/guangzhou/gz9/4.jpg',
+      '/static/guangzhou/gz9/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz9/7.mp4',
+      '/static/guangzhou/gz9/8.mp4',
+     ]
+  },
+  {
+    id: 'gz10',
+    title: '车模 康康 178D', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz10/1.jpg',
+      '/static/guangzhou/gz10/2.jpg',
+      '/static/guangzhou/gz10/3.jpg',
+      '/static/guangzhou/gz10/4.jpg',
+      '/static/guangzhou/gz10/5.jpg',
+      '/static/guangzhou/gz10/6.jpg',
+      '/static/guangzhou/gz10/7.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz10/8.mp4',
+      '/static/guangzhou/gz10/9.mp4',
+     ]
+  },
+  {
+    id: 'gz11',
+    title: 'Gz小魔仙🏠178', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz11/1.jpg',
+      '/static/guangzhou/gz11/2.jpg',
+      '/static/guangzhou/gz11/3.jpg',
+      '/static/guangzhou/gz11/4.jpg',
+      '/static/guangzhou/gz11/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz11/6.mp4',
+      '/static/guangzhou/gz11/7.mp4',
+      '/static/guangzhou/gz11/8.mp4',
+     ]
+  },
+  {
+    id: 'gz12',
+    title: 'Gz小魔仙🏠露露', 
+    description: `
+    空姐 露露 身高171熊f真实00年 海航空姐兼职 肤白貌美大长腿 皮肤白皙 A4腰身 极品身材 初次兼职 性格温柔 服务有耐心 不催不事。。。。
+    `, 
+    image: [
+      '/static/guangzhou/gz12/1.jpg',
+      '/static/guangzhou/gz12/2.jpg',
+      '/static/guangzhou/gz12/3.jpg',
+      '/static/guangzhou/gz12/4.jpg',
+    ],
+    video: [
+      '/static/guangzhou/gz12/5.mp4',
+
+     ]
+  },
+  {
+    id: 'gz13',
+    title: 'Gz小魔仙🏠 小🐟', 
+    description: `
+       小🐟 𝐇𝐞𝐢𝐠𝐡𝐭身高/168 • 𝐁𝐮𝐬𝐭胸围/ D • 𝐀𝐠𝐞年龄/04 前凸后翘 皮肤细腻 性格温柔开朗高素质 兼职短开 积极配合 声音甜美 可御可甜
+    `, 
+    image: [
+      '/static/guangzhou/gz13/1.jpg',
+      '/static/guangzhou/gz13/2.jpg',
+      '/static/guangzhou/gz13/3.jpg',
+      '/static/guangzhou/gz13/4.jpg',
+      '/static/guangzhou/gz13/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz13/6.mp4',
+      '/static/guangzhou/gz13/7.mp4',
+     ]
+  },
+  {
+    id: 'gz14',
+    title: 'Gz小魔仙🏠176御姐', 
+    description: `
+    丽途国际 176cm 55kg D杯 极品网红 私处会吸夹 有概率潮喷  天然🐻 氵多 人间水蜜桃🍑三点粉嫩  穿衣气质  豪宅工作室  圈外兼职同城可见本人
+    `, 
+    image: [
+      '/static/guangzhou/gz14/1.jpg',
+      '/static/guangzhou/gz14/2.jpg',
+      '/static/guangzhou/gz14/3.jpg',
+      '/static/guangzhou/gz14/4.jpg',
+      '/static/guangzhou/gz14/5.jpg',
+      '/static/guangzhou/gz14/6.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz14/7.mp4',
+      '/static/guangzhou/gz14/8.mp4',
+     ]
+  },
+  {
+    id: 'gz15',
+    title: 'Gz小魔仙🏠180车模', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz15/1.jpg',
+      '/static/guangzhou/gz15/2.jpg',
+      '/static/guangzhou/gz15/3.jpg',
+      '/static/guangzhou/gz15/4.jpg',
+      '/static/guangzhou/gz15/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz15/6.mp4',
+      '/static/guangzhou/gz15/7.mp4',
+      '/static/guangzhou/gz15/8.mp4',
+     ]
+  },
+  {
+    id: 'gz16',
+    title: '广州超模首开 身高182 胸f 年龄00', 
+    description: `
+    广州真实超模极品硬5万 五星级公寓 • 𝐇𝐞𝐢𝐠𝐡𝐭身高/182 • 𝐖𝐞𝐢𝐠𝐡𝐭体重/92 • 𝐁𝐮𝐬𝐭胸围/胸f • 𝐀𝐠𝐞年龄/00 • 𝐎𝐜𝐜𝐮𝐩𝐚𝐭𝐢𝐨𝐧职业/ 极品超模 全身无赘肉 S型身材 本人比照片漂亮 皮肤雪白 💎 贴心小女友 大长腿 💎 气质大牌  皮肤白嫩 只接素质🉑
+    `, 
+    image: [
+      '/static/guangzhou/gz16/1.jpg',
+      '/static/guangzhou/gz16/2.jpg',
+      '/static/guangzhou/gz16/3.jpg',
+      '/static/guangzhou/gz16/4.jpg',
+      '/static/guangzhou/gz16/5.jpg',
+      '/static/guangzhou/gz16/6.jpg',
+      '/static/guangzhou/gz16/7.jpg',
+      '/static/guangzhou/gz16/8.jpg',
+      '/static/guangzhou/gz16/9.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz16/10.mp4',
+      '/static/guangzhou/gz16/11.mp4',
+     ]
+  },
+  {
+    id: 'gz17',
+    title: 'Gz小魔仙新人180琪琪', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz17/1.jpg',
+      '/static/guangzhou/gz1/2.jpg',
+      '/static/guangzhou/gz17/3.jpg',
+      '/static/guangzhou/gz17/4.jpg',
+      '/static/guangzhou/gz17/5.jpg',
+      '/static/guangzhou/gz17/6.jpg',
+      '/static/guangzhou/gz17/7.jpg',
+    
+    ],
+    video: [
+
+      '/static/guangzhou/gz17/8.mp4',
+      '/static/guangzhou/gz17/9.mp4',
+     ]
+  },
+  {
+    id: 'gz18',
+    title: 'Gz小魔仙🏠广州05宝宝', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz18/1.jpg',
+      '/static/guangzhou/gz18/2.jpg',
+      '/static/guangzhou/gz18/3.jpg',
+      '/static/guangzhou/gz18/4.jpg',
+      '/static/guangzhou/gz18/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz18/6.mp4',
+      '/static/guangzhou/gz18/7.mp4',
+     ]
+  },
+  {
+    id: 'gz19',
+    title: 'Gz小魔仙🏠176模特', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz19/1.jpg',
+      '/static/guangzhou/gz19/2.jpg',
+      '/static/guangzhou/gz19/3.jpg',
+      '/static/guangzhou/gz19/4.jpg',
+      '/static/guangzhou/gz19/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz19/6.mp4',
+      '/static/guangzhou/gz19/7.mp4',
+      '/static/guangzhou/gz19/8.mp4',
+     ]
+  },
+  {
+    id: 'gz20',
+    title: 'Gz小魔仙🏠广州176瑶瑶', 
+    description: `
+    `, 
+    image: [
+      '/static/guangzhou/gz20/1.jpg',
+      '/static/guangzhou/gz20/2.jpg',
+      '/static/guangzhou/gz20/3.jpg',
+      '/static/guangzhou/gz20/4.jpg',
+      '/static/guangzhou/gz20/5.jpg',
+    
+    ],
+    video: [
+      '/static/guangzhou/gz20/6.mp4',
+      '/static/guangzhou/gz20/7.mp4',
+      '/static/guangzhou/gz20/8.mp4',
+     ]
+  },
+  
+]
+
+export default girls
